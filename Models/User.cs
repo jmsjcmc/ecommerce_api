70eb4241f084f4edd61595024b16a5f587e3b6e3
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-01-13 09:17:38 +0800

    Added gitignore

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace ecommerce_api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public string Password { get; set; }
+        public DateTime Datecreated { get; set; }
+    }
+}
